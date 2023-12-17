a01ac194981aae880f16d30a5ffd4bba4816388a
Leif Liddy <leif.liddy@gmail.com>
2023-12-17 02:17:04 +0100

    minor update

--- build.sh
+++ build.sh
@@ -142,17 +142,12 @@ make_image() {
     # remove resolv.conf symlink -- this causes issues with arch-chroot
     rm -f $mnt_image/etc/resolv.conf
 
-    # need to generate a machine-id so that a BLS entry can be created below
-    echo -e '\n### Running systemd-machine-id-setup'
-    chroot $mnt_image systemd-machine-id-setup
-    chroot $mnt_image echo "KERNEL_INSTALL_MACHINE_ID=$(cat /etc/machine-id)" > /etc/machine-info
-
     echo -e '\n### Generating EFI bootloader'
     arch-chroot $mnt_image create-efi-bootloader
 
     echo -e '\n### Generating GRUB config'
     arch-chroot $mnt_image grub2-editenv create
-    rm -f $mnt_image/etc/kernel/cmdline
+    sed -i "s/ROOT_UUID_PLACEHOLDER/$ROOT_UUID/" $mnt_image/etc/kernel/cmdline
     sed -i "s/BOOT_UUID_PLACEHOLDER/$BOOT_UUID/" $mnt_image/boot/efi/EFI/rocky/grub.cfg
     # /etc/grub.d/30_uefi-firmware creates a uefi grub boot entry that doesn't work on this platform
     chroot $mnt_image chmod -x /etc/grub.d/30_uefi-firmware
